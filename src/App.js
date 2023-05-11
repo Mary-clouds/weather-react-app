01830bcbb36e4b5c6331b41d09565000eaabb73b
Mary-clouds <marynevermann@gmail.com>
2023-05-11 10:51:45 +0200

    add some css in App.css

--- src/App.js
+++ src/App.js
@@ -4,112 +4,128 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Weather">
-          <form className="mb-3">
-            <WeatherSearch />
-          </form>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <div className="Weather">
+            <h1>
+              <WeatherSearch />
+            </h1>
 
-          <div className="citiesinformations">
-            <h1>Berlin</h1>
-            <ul className="float-left">
-              <li>LAst updated: Monday 7:13</li>
-              <li>Sunny</li>
-            </ul>
-          </div>
-
-          <div className="row">
-            <div class="col-6">
-              <div class="clearfix weather-temperature">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                  alt="sun"
-                  class="float-left img-fluid mx-auto"
-                />
-                <div class="float-left">
-                  <strong>25</strong>
-                  <span class="units">
-                    <a href=" ">°C</a>|<a href=" ">°F</a>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="col-6">
-              <ul>
-                <li>
-                  feels like <span>28°C</span>
-                </li>
-                <li>Wind: 14 km/h</li>
-                <li>Humidity: 17%</li>
+            <div className="Citiesinformations">
+              <h2>Berlin</h2>
+              <ul className="float-left">
+                <li>Last updated: Monday 7:13</li>
+                <li>Sunny</li>
               </ul>
             </div>
-          </div>
-          <br>
+
+            <div className="row">
+              <div className="col-3">
+                <div className="clearfix weather-temperature">
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="sun"
+                    class="float-left img-fluid mx-auto"
+                  />
+                  <div className="float-left">
+                    <strong>25</strong>
+                    <span class="units">
+                      <a href=" ">°C</a>|<a href=" ">°F</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-3">
+                <ul>
+                  <li>
+                    feels like <span>28°C</span>
+                  </li>
+                  <li>Wind: 14 km/h</li>
+                  <li>Humidity: 17%</li>
+                </ul>
+              </div>
+            </div>
+            <br />
             <div className="Forecast">
-              <div class="row justify-content-center">
-                <div class="col">
-                  <div class="card h-100" style="width:10rem;">
+              <div className="row ">
+                <div className="col">
+                  <div
+                    className="card h-100"
+                    style={{ fontSize: "width:10rem;" }}
+                  >
                     <img
                       src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
                       alt="rain"
                       class="img-fluid mx-auto text-centrer"
                     />
-                    <div class="card-body text-center">
+                    <div className="card-body text-center">
                       <h5 class="card-body text-center">Monday</h5>
                       <p class="card-text text-center">Rain</p>
                     </div>
                   </div>
                 </div>
-                <div class="col">
-                  <div class="card h-100" style="width:10rem;">
+                <div className="col">
+                  <div
+                    className="card h-100"
+                    style={{ fontSize: "width:10rem;" }}
+                  >
                     <img
                       src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
                       alt="sun"
                       class="img-fluid mx-auto text-centrer"
                     />
-                    <div class="card-body text-center">
+                    <div className="card-body text-center">
                       <h5 class="card-body text-center">Tuesday</h5>
                       <p class="card-text text-center">Sunny</p>
                     </div>
                   </div>
                 </div>
 
-                <div class="col">
-                  <div class="card h-100" style="width:10rem;">
+                <div className="col">
+                  <div
+                    className="card h-100"
+                    style={{ fontSize: "width:10rem;" }}
+                  >
                     <img
                       src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
                       alt="Cloud"
                       class="img-fluid mx-auto text-centrer"
                     />
-                    <div class="card-body text-center">
+                    <div className="card-body text-center">
                       <h5 class="card-body text-center">Wednesday</h5>
                       <p class="card-text text-center">Cloudy</p>
                     </div>
                   </div>
                 </div>
-                <div class="col">
-                  <div class="card h-100" style="width:10rem;">
+                <div className="col">
+                  <div
+                    className="card h-100"
+                    style={{ fontSize: "width:10rem;" }}
+                  >
                     <img
                       src="https://ssl.gstatic.com/onebox/weather/64/thunderstorms.png"
                       alt="thunder"
                       class="img-fluid mx-auto text-centrer"
                     />
-                    <div class="card-body text-center">
+                    <div className="card-body text-center">
                       <h5 class="card-body text-center">Thursday</h5>
                       <p class="card-text text-center">Thunderstorm</p>
                     </div>
                   </div>
                 </div>
 
-                <div class="col">
-                  <div class="card h-100" style="width:10rem;">
+                <div className="col">
+                  <div
+                    className="card h-100"
+                    style={{ fontSize: "width:10rem;" }}
+                  >
                     <img
                       src="https://ssl.gstatic.com/onebox/weather/64/snow.png"
                       alt="snow"
                       class="img-fluid mx-auto text-centrer"
                     />
-                    <div class="card-body text-center">
+                    <div className="card-body text-center">
                       <h5 class="card-body text-center">Friday</h5>
                       <p class="card-text text-center">snow</p>
                     </div>
@@ -117,18 +133,21 @@ function App() {
                 </div>
               </div>
             </div>
-          </br>
-          <footer>
-            <strong>
-              coded by Mary Nevermann
-              <a href=" " target="_blank" rel="noopener norefereer">
-                open source on Github
-              </a>
-              <a href=" " target="_blank" rel="noopener norefereer">
-                hosted on Netlify
-              </a>
-            </strong>
-          </footer>
+
+            <footer className="Footer">
+              <strong>
+                coded by Mary Nevermann
+                <br />
+                <a href=" " target="_blank" rel="noopener norefereer">
+                  open source on Github
+                </a>
+                <br />
+                <a href=" " target="_blank" rel="noopener norefereer">
+                  hosted on Netlify
+                </a>
+              </strong>
+            </footer>
+          </div>
         </header>
       </div>
     </div>
